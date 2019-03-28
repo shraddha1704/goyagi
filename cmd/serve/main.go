ff8c228dd622e61dc25ce693b18e9c1f3ceddd19
shraddha <shraddha1704@gmail.com>
2019-03-28 11:35:25 -0700

    feat(database): add pg database

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -4,17 +4,23 @@ import (
 	"net/http"
 
 	"github.com/lob/logger-go"
+	"github.com/shraddha1704/goyagi/pkg/application"
 	"github.com/shraddha1704/goyagi/pkg/server"
 )
 
 func main() {
 	log := logger.New()
 
-	srv := server.New()
+	app, err := application.New()
+	if err != nil {
+		log.Err(err).Fatal("failed to initialize application")
+	}
 
-	log.Info("server started")
+	srv := server.New(app)
 
-	err := srv.ListenAndServe()
+	log.Info("server started", logger.Data{"port": app.Config.Port})
+
+	err = srv.ListenAndServe()
 	if err != nil && err != http.ErrServerClosed {
 		log.Err(err).Fatal("server stopped")
 	}
--- pkg/server/server.go
+++ pkg/server/server.go
@@ -7,11 +7,13 @@ import (
 
 	"github.com/labstack/echo"
 	"github.com/lob/logger-go"
+	"github.com/shraddha1704/goyagi/pkg/application"
 	"github.com/shraddha1704/goyagi/pkg/health"
 	"github.com/shraddha1704/goyagi/pkg/signals"
 )
 
-func New() *http.Server {
+// New returns a new HTTP server with the registered routes.
+func New(app application.App) *http.Server {
 	log := logger.New()
 
 	e := echo.New()
@@ -19,7 +21,7 @@ func New() *http.Server {
 	health.RegisterRoutes(e)
 
 	srv := &http.Server{
-		Addr:    fmt.Sprintf(":%d", 3000),
+		Addr:    fmt.Sprintf(":%d", app.Config.Port),
 		Handler: e,
 	}
 
